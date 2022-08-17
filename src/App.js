6b78d8c24d9d05e4049f81073ed50a76f10bba3e
vamsi805 <vamsibobba805@gmail.com>
2022-08-17 20:38:06 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import { getPosts } from './actions/posts';
 import useStyles from './styles';
-import memories from './images/memories.png';
+
 
 const App = () => {
   const [currentId, setCurrentId] = useState(0);
